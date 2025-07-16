ed7f24e9d66c4e9d8cea4a862c56b76251488110
Tejesh <tejeshchintada22@gmail.com>
2025-07-16 11:38:44 +0530

    changed route permissions

--- farmer_app_backend/app/utils/route_permissions.py
+++ farmer_app_backend/app/utils/route_permissions.py
@@ -3,7 +3,7 @@ ROUTE_ROLE_PERMISSIONS= {
     "/remove-role": ["ADMIN", "SUPER_USER"],
     "/farmer": ["ADMIN", "SUPER_USER"],
     "/farm": ["ADMIN", "SUPER_USER"],
-    "/schedules": ["ADMIN", "SUPER_USER"],
+    "/schedule": ["ADMIN", "SUPER_USER"],
     "/schedules/due": ["USER", "ADMIN", "SUPER_USER"],
     "/farmers/by-crop": ["USER", "ADMIN", "SUPER_USER"],
     "/bill": ["USER", "ADMIN", "SUPER_USER"],
