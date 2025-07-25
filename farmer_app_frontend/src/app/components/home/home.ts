51d6acd8573789182c5ce5313204180c64579095
Tejesh <tejeshchintada22@gmail.com>
2025-07-25 11:05:15 +0530

    frontend and backend completed

--- farmer_app_frontend/src/app/components/home/home.ts
+++ farmer_app_frontend/src/app/components/home/home.ts
@@ -6,9 +6,6 @@ import { FarmersByCrop } from '../farmers-by-crop/farmers-by-crop';
 import { AddRole } from '../add-role/add-role';
 import { RemoveRole } from '../remove-role/remove-role';
 import { Getbill } from '../getbill/getbill';
-import { Farmers } from '../farmers/farmers';
-import { Farms } from '../farms/farms';
-import { Schedules } from '../schedules/schedules';
 import { SchedulesDue } from '../schedules-due/schedules-due';
 import { CreateSchedule } from '../create-schedule/create-schedule';
 import { CreateFarm } from '../create-farm/create-farm';
@@ -19,7 +16,7 @@ import { FarmersWithCrop } from '../farmers-with-crop/farmers-with-crop';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [[CommonModule, Navbar, FarmersByCrop, FarmersWithCrop, AddRole, RemoveRole, Getbill, Farmers, Farms, Schedules, SchedulesDue, CreateSchedule, CreateFarm,CreateFarmer]],
+  imports: [[CommonModule, Navbar, FarmersByCrop, FarmersWithCrop, AddRole, RemoveRole, Getbill, SchedulesDue, CreateSchedule, CreateFarm,CreateFarmer]],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
